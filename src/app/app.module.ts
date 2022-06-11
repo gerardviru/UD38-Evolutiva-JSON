f1f29f56e68676d80e41079076712bb8150ad247
Gerard Viñuela <gerardviru@gmail.com>
2022-06-11 19:19:04 +0200

    UD38 Evolutiva JSON

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CharactersComponent } from './characters/characters.component';
+import { AboutComponent } from './about/about.component';
+import { DatosComponent } from './datos/datos.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CharactersComponent,
+    AboutComponent,
+    DatosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
